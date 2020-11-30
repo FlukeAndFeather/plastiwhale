bf68ca1a31554e7d4f150b028cc429ab1c8b1c5e
shirelkr <skahaner@stanford.edu>
2020-11-30 13:17:36 -0800

    have source code now

--- src/MAKE.R
+++ src/MAKE.R
@@ -1,15 +1,19 @@
 # MAKEFILE AKA src/MAKE.R
 
-# 1. Collect data
+# 1. Collect data using collect_lunges
+
 # If [RDS files] don't exist:
 source("src/collect_lunges.R")
 # Else:
 deployments <- readRDS("data/output/deployments.RDS")
+lunges <- readRDS("data/output/lunges.RDS")
 # same for lunges
 
 # 2. Diel cycle calculations
+source("src/dielperiod.R")
 # If RDS files exist ...
 # Else ...
 
 # 3. Feeding metric calculations
+source("src/depth_buckets.R")
 # ...
